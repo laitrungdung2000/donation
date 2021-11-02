61df95f4eb12e767267a3ae11a0d92a31048efb3
laitrungdung2000 <laidung29022000@gmail.com>
2021-11-02 14:37:23 +0700

    b4

--- app/src/main/java/com/example/donation/MainActivity.java
+++ app/src/main/java/com/example/donation/MainActivity.java
@@ -52,36 +52,36 @@ public class MainActivity extends Base {
                         .setAction("Action", null).show();
             }
         });
-//        donateButton = (Button) findViewById(R.id.donateButton);
-//        paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
-//        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-//        amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
+        donateButton = (Button) findViewById(R.id.donateButton);
+        paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
 //        amountText = (EditText) findViewById(R.id.paymentAmount);
 //        amountTotal = (TextView) findViewById(R.id.totalSoFar);
-//        amountPicker.setMinValue(0);
-//        amountPicker.setMaxValue(1000);
-//        progressBar.setMax(10000);
+        amountPicker.setMinValue(0);
+        amountPicker.setMaxValue(1000);
+        progressBar.setMax(10000);
 //        amountTotal.setText("$0");
     }
 
     public void donateButtonPressed (View view)
     {
-//        String method = paymentMethod.getCheckedRadioButtonId() == R.id.PayPal ?
-//                "PayPal" : "Direct";
-//        int donatedAmount = amountPicker.getValue();
-//        if (donatedAmount == 0)
-//        {
+        String method = paymentMethod.getCheckedRadioButtonId() == R.id.PayPal ?
+                "PayPal" : "Direct";
+        int donatedAmount = amountPicker.getValue();
+        if (donatedAmount == 0)
+        {
 //            String text = amountText.getText().toString();
 //            if (!text.equals(""))
 //                donatedAmount = Integer.parseInt(text);
-//        }
-//        if (donatedAmount > 0)
-//        {
-//            newDonation(new Donation(donatedAmount, method));
-//            progressBar.setProgress(totalDonated);
-//            String totalDonatedStr = "$" + totalDonated;
+        }
+        if (donatedAmount > 0)
+        {
+            newDonation(new Donation(donatedAmount, method));
+            progressBar.setProgress(totalDonated);
+            String totalDonatedStr = "$" + totalDonated;
 //            amountTotal.setText(totalDonatedStr);
-//        }
+        }
         Log.v("Donate", "Really got the donate button");
     }
 
